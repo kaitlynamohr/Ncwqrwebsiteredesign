e9e153ae05a8c6aa59f4be08876fbaaec3bd273d
kam-413 <kmohr1@heidelberg.edu>
2026-03-25 12:10:19 -0400

    add tributes to route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,7 @@ import { Root } from './components/Root';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { History } from './pages/History';
+import { Tributes } from './pages/Tributes';
 import { Monitoring } from './pages/Monitoring';
 import { Research } from './pages/Research';
 import { Publications } from './pages/Publications';
@@ -23,6 +24,7 @@ export const router = createBrowserRouter([
       { index: true, Component: Home },
       { path: 'about', Component: About },
       { path: 'history', Component: History },
+      { path: 'tributes', Component: Tributes },
       { path: 'monitoring', Component: Monitoring },
       { path: 'research', Component: Research },
       { path: 'publications', Component: Publications },
